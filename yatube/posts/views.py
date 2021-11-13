0038f19f018477b6f3c414fe7d176fbd79c5d17b
Mikita Navicki <bulbanik91@gmail.com>
2021-11-13 15:40:23 +0300

    Решена ошибка редактирования шаблона group_list.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -21,7 +21,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.all()
+    posts = Post.objects.filter(group=group).all()
     paginator = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
