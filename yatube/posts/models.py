6b91630d7f64fc25459b34d712b001c3dd2d1696
Mikita Navicki <bulbanik91@gmail.com>
2021-11-13 15:23:16 +0300

    В процессе прохождения pytest

---
+++ yatube/posts/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['-pub_date']
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
---
+++ yatube/posts/views.py
@@ -0,0 +1,102 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import PostForm
+from .models import Group, Post, User
+
+
+@login_required
+def index(request):
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    template = 'posts/index.html'
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    template = 'posts/group_list.html'
+    context = {
+        'group': group,
+        'posts': posts,
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = Post.objects.filter(author=author).all()
+    count_user_posts = post_list.count()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    title = username
+    template = 'posts/profile.html'
+    context = {
+        'author': author,
+        'count_user_posts': count_user_posts,
+        'page_obj': page_obj,
+        'title': title,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    user = get_object_or_404(User, username=post.author)
+    count = Post.objects.filter(author_id=user).all().count()
+    template = 'posts/post_detail.html'
+    context = {
+        'post': post,
+        'count': count,
+    }
+    return render(request, template, context)
+
+
+@login_required
+def post_create(request):
+    template = 'posts/create_post.html'
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            form.save()
+            return redirect('posts:profile', post.author)
+        return render(request, template, {'form': form})
+    form = PostForm()
+    title = 'Добавить запись'
+    return render(request, template, {'form': form, 'title': title})
+
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            form.save()
+            return redirect('posts:post_detail', post_id)
+        return render(request, 'posts/create_post.html', {'form': form})
+
+    is_edit = True
+    template = 'posts/create_post.html'
+    context = {
+        'form': form,
+        'is_edit': is_edit,
+    }
+    return render(request, template, context)
