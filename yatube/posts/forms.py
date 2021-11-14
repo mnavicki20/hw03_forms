a8504d378a6c5c73b0b2d192bed5d330857d4ec4
Mikita Navicki <bulbanik91@gmail.com>
2021-11-14 22:16:56 +0300

    Правки по ревью v.1

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -7,3 +7,5 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group')
+        verbose_name = 'Публикация пользователя'
+        verbose_name_plural = 'Публикации пользователей'
