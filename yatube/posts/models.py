68e0964aef462c45e30aebd30d7a4c0b01f560d8
Mikita Navicki <bulbanik91@gmail.com>
2021-11-14 22:23:31 +0300

    Правки по ревью v.2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -20,11 +20,11 @@ class Post(models.Model):
         null=True,
     )
 
-    def __str__(self):
-        return self.text
-
     class Meta:
         ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text
 
 
 class Group(models.Model):
