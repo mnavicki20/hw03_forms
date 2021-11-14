999d3a975f7c624d5cf51a92e5e50744663bdacc
Mikita Navicki <bulbanik91@gmail.com>
2021-11-14 22:28:37 +0300

    Правки по ревью v.3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -78,6 +78,7 @@ def post_create(request):
     return render(request, template, {'form': form, 'title': title})
 
 
+@login_required
 def post_edit(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     form = PostForm(instance=post)
