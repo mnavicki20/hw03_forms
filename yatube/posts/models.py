bf22d876ee3ee1ef172e4aea3bf2fbe57ea05a8a
Mikita Navicki <bulbanik91@gmail.com>
2021-11-20 15:52:19 +0300

    Тестирование verbose_name и help_text

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,12 +5,19 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(
+        verbose_name='Текст публикации',
+        help_text='Введите текст публикации'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts',
+        verbose_name='Автор публикации',
     )
     group = models.ForeignKey(
         'Group',
@@ -18,13 +25,15 @@ class Post(models.Model):
         related_name='posts',
         blank=True,
         null=True,
+        verbose_name='Группа публикации',
+        help_text='Укажите группу публикации',
     )
 
     class Meta:
         ordering = ['-pub_date']
 
     def __str__(self):
-        return self.text
+        return self.text[:15]
 
 
 class Group(models.Model):
--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -18,7 +18,8 @@ class PostModelTest(TestCase):
         )
         cls.post = Post.objects.create(
             author=cls.user,
-            text='Тестовая группа',
+            text='Тестовый текст публикации',
+            pub_date='Тестоавая дата публикации',
         )
 
     def test_model_group_has_correct_object_names(self):
@@ -32,3 +33,29 @@ class PostModelTest(TestCase):
         post = PostModelTest.post
         expected_post_name = post.text[:15]
         self.assertEqual(expected_post_name, str(post))
+
+    def test_verbose_name(self):
+        """verbose_name в полях сопадает с ожидаемым."""
+        post = PostModelTest.post
+        field_verboses = {
+            'author': 'Автор публикации',
+            'text': 'Текст публикации',
+            'pub_date': 'Дата публикации',
+            'group': 'Группа публикации',
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    post._meta.get_field(field).verbose_name, expected_value)
+
+    def test_help_text(self):
+        """help_text в полях сопадает с ожидаемым."""
+        post = PostModelTest.post
+        field_help_texts = {
+            'text': 'Введите текст публикации',
+            'group': 'Укажите группу публикации',
+        }
+        for field, expected_value in field_help_texts.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    post._meta.get_field(field).help_text, expected_value)
